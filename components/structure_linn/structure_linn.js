0b68257abe774c54e0fea1cf47c76fe9d6dd8900
peterssn <linn.sa.petersson@gmail.com>
2023-11-29 14:31:15 +0100

    put semicolon on every line/ closed every line

--- components/structure_linn/structure_linn.js
+++ components/structure_linn/structure_linn.js
@@ -1,19 +1,19 @@
 // Jag förstod inte helt vad  man skulle göra, "skapa divvar" jag antar att detta ska ske i js ?
 
-let header = document.createElement("header")
+let header = document.createElement("header");
 
-let shoe_nav = document.createElement("nav")
+let shoe_nav = document.createElement("nav");
 
-let filter_nav = document.createElement("nav")
+let filter_nav = document.createElement("nav");
 
-let main = document.createElement("main")
+let main = document.createElement("main");
 
-let nav_div = document.createElement("div")
+let nav_div = document.createElement("div");
 
-let article  = document.createElement("article")
+let article  = document.createElement("article");
 
-let footer  = document.createElement("footer")
+let footer  = document.createElement("footer");
 
-let shoe_div = document.createElement("div")
+let shoe_div = document.createElement("div");
 
-let about_us_section = document.createElement("section")
+let about_us_section = document.createElement("section");
