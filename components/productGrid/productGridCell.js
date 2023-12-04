843b8c0e10a9d43643f93c15c1f55ea9aaab2199
yuki9k <tornlund.freja@gmail.com>
2023-12-04 15:59:14 +0100

    Added flags and innerHTML for productcell

--- components/productGrid/productGridCell.js
+++ components/productGrid/productGridCell.js
@@ -5,7 +5,17 @@ function renderProductCell(parent, array) {
         const productCell = document.createElement("div");
         productCell.classList.add("product-cell");
         productCell.innerHTML = `
-        
+        <div class="flag">
+            <img src="../../media/flags/${shoe.country_id}.png">
+        </div>
+        <div class="shoe-pic">
+            <img src="../../media/skobilder/${shoe.file_name}.jpg">
+        </div>
+        <div class="shoe-information">
+            <h3 class="type">SNEAKERS</h3>
+            <h1 class="name">${shoe.name}</h1>
+            <h2 class="price">${shoe.price}</h2>
+        </div>
         `;
         parent.appendChild(productCell);
     }
