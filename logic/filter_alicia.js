3946e4523e0be9c061b433b92d6eb44d60be77f7
Aliciaflasch <59962026+Aliciaflasch@users.noreply.github.com>
2023-12-05 22:45:24 +0100

    Add files via upload

---
+++ logic/filter_alicia.js
@@ -0,0 +1,12 @@
+
+"use strict";
+
+function filter_Alicia(a_shoelist, i_min, i_max) {
+    let a_filteredshoelist = [];
+    for (let myshoe of a_shoelist) {
+        if (myshoe.price >= i_min && myshoe.price <= i_max) {
+            a_filteredshoelist.push(myshoe);
+        }
+    }
+    return a_filteredshoelist;
+}
