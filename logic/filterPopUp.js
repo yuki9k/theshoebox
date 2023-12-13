7d8844248dd19aa4da1e1d1b9a9c20a8b0c10991
Bullwhips <epicj4444@gmail.com>
2023-12-13 10:17:42 +0100

    Added a second filterpopup logic

--- logic/filterPopUp.js
+++ logic/filterPopUp.js
@@ -54,35 +54,7 @@ maxPriceSlider.addEventListener('mouseup', filter)
 
 
 
-// To be added to every button/checkbox
-function filter() {
-    country_ids = get_checked_ids(COUNTRIES);
-    shoe_type_ids = get_checked_ids(KINDS);
 
-
-    noCountrySelected = country_ids.length === 0;
-    noShoeTypeSelected = shoe_type_ids.length === 0;
-
-
-    // Find all shoes that have a country id that is in the country_ids array
-    new_arr = []
-    for (let i = 0; i < SHOES.length; i++) {
-
-
-        isFromCountry = noCountrySelected || country_ids.includes(SHOES[i].country_id);
-        isBelowMaxPrice = maxPriceSlider.value >= SHOES[i].price
-        isCorrectShoeType = noShoeTypeSelected || shoe_type_ids.includes(SHOES[i].kind_id);; // TODO
-
-
-        if (isFromCountry && isBelowMaxPrice && isCorrectShoeType) {
-            new_arr.push(SHOES[i])
-        }
-
-
-    }
-
-    return new_arr;
-}
 
 
 function get_checked_ids(arr) {
