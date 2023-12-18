5eec6a57ea102059aace64bfe5040dd2816c5fa8
rimnell <viktor03rimnell@gmail.com>
2023-12-18 09:56:47 +0100

    completed review bar

--- logic/getShoeReviews.js
+++ logic/getShoeReviews.js
@@ -5,7 +5,15 @@ function renderShoeReviews(shoeReviews) {
   for (let review of shoeReviews) {
     const grade = document.createElement("div");
     grade.innerHTML = `
-    <p>Review: ${review.rev}</p>
+  <p>
+    <img src="/Users/viktorrimnell/Documents/GitHub/theshoebox/media/icons/star-full-icon-orange.png" alt="star">
+    <article>${review.score}</article>
+  </p>
+  <section>
+    <p><strong>Review:</strong></p>
+    <p>${review.rev}</p>
+  </section>
+ 
   `;
 
     document.getElementById("grade").appendChild(grade);
