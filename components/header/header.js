9c71bcc8db08dc458284eb3b63919bd4fd684a23
Bullwhips <epicj4444@gmail.com>
2023-12-18 20:29:11 +0100

    Changes to filter popup

--- components/header/header.js
+++ components/header/header.js
@@ -20,3 +20,4 @@ function renderHeader (header) {
     </div>
     `;
 }
+
--- index.js
+++ index.js
@@ -7,3 +7,15 @@ createAboutUsPopUp(document.querySelector("#about-us-button"));
 renderfilterbar(document.querySelector("#filter-bar"));
 render_filter_pop_up(document.querySelector("#filter-button"));
 renderProductCell(structureContainers.productGrid, SHOES);
+document.querySelector("header").addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector("#filterMain").classList.remove("filter-popup-show");
+  });
+  document.querySelector("#filter-bar").addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector("#filterMain").classList.remove("filter-popup-show");
+  });
+  document.querySelector("main").addEventListener('click', (e) => {
+    e.stopPropagation();
+    document.querySelector("#filterMain").classList.remove("filter-popup-show");
+  });
