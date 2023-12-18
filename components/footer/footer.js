30a67f9f524074e4522d8afb9ad1aa90461dc960
majasielski <143742130+majasielski@users.noreply.github.com>
2023-12-18 09:32:42 +0100

    Added footer components

---
+++ components/footer/footer.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function renderfilterbar (parent) {
+let footer = document.createElement ("div");
+footer.classList.add ("footer-left")
+footer.innerHTML = `
+
+<p>INFORMATION</p>
+
+<div class="footer-nav">
+<nav> <a href="">ABOUT US</a></nav>
+</div>
+</div>        
+<div class="footer-right">
+<p>Sign up to get 10% off your first order</p>
+<div class="email-container">
+<input type="SIGN UP" placeholder="ENTER EMAIL" class="email-container-1">
+<input type="submit" value="SIGN UP" class="footer-email.btn">
+<p>You´ll be the first to know about exclusive products launches, sales and much more. </p>
+</div>
+
+` ;
+parent.appendChild(footer);
+};
