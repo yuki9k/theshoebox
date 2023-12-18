c440cac84ffbaaa75d193887ec8e852f0bd76cb4
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2023-12-18 10:37:55 +0100

    changed the innerhtml and cleaned css file

--- components/filterbar/filterbar.js
+++ components/filterbar/filterbar.js
@@ -2,18 +2,18 @@
 
 function renderfilterbar (parent) {
 let filterBar = document.createElement ("div");
-filterBar.classList.add ("bar")
+filterBar.classList.add ("bar");
 filterBar.innerHTML = `
 <div class="navleft">
-        <div> <img src="filterbar.png" alt=""></div>
-        </div>  
-        
-              
-        <div class="navright">
-          <p>View</p>
-          <div> <img src="tkol.png" alt=""></div>
-          <div> <img src="fcol.png" alt=""></div>
-        </div>
-` ;
+  <div><img src="filterbar.png" alt=""></div>
+</div>  
+             
+<div class="navright">
+  <p>View</p>
+  <div><img src="tkol.png" alt=""></div>
+  <div><img src="fcol.png" alt=""></div>
+</div>
+`;
+
 parent.appendChild(filterBar);
 };
