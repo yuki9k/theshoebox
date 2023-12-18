39b4885a18e685a758a73923afe6c8035ba7f6f6
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2023-12-18 15:29:41 +0100

    fixed header

--- components/header/header.js
+++ components/header/header.js
@@ -6,13 +6,17 @@ function renderHeader (header) {
     <div id="free-shipping">Free shipping on all orders in EU</div>
 
     <div id="items">
-        <div id="search-bar"><img id="search-icon" src="media/icons/search-icon.png" alt="">Search</div>
+        <div id="search-bar">
+            <div id="search-icon"></div>
+            <p>Search</p>
+        </div>
+        
         <h1 id="page-title">THE.SHOE.BOX</h1>
         
         <div id="header-icons">
-            <img id="user-icon" src="media/icons/user-icon-orange.png" alt="">
-            <img id="heart-icon" src="media/icons/heart-thin-icon-oranges.png" alt="">
-            <img id="bag-icon" src="media/icons/bag-icon-orange.png" alt="">
+            <div id="user-icon"></div>
+            <div id="heart-icon"></div>
+            <div id="bag-icon"></div>
     </div>
     `;
 }
