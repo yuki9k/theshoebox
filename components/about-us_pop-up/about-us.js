db72fa85420535ac8dbf15f1e4f8a5f622d5affe
yuki9k <tornlund.freja@gmail.com>
2023-12-18 12:24:50 +0100

    Finally fixed about us pop up and added group photo

--- components/about-us_pop-up/about-us.js
+++ components/about-us_pop-up/about-us.js
@@ -4,11 +4,7 @@ function createAboutUsPopUp(parent) {
   let aboutUsPopUp = document.createElement("div");
   aboutUsPopUp.setAttribute("id", "about-us-popup");
   aboutUsPopUp.innerHTML = `
-  <div id="footer-left">
-
-  <button>ABOUT US</button> 
-  <div id="about-us-popup">
-
+  <div id="about-us-modal">
   <div id="popup-text">
      <h1>About Us</h1>
      <p>We are the team behind The.Shoe.Box. We take the latest shoe trends and turn them into inspiring fashion in different styles, sizes and for all kinds of occasions. With our website you can easily find shoes to several occasions.</p>
@@ -17,37 +13,14 @@ function createAboutUsPopUp(parent) {
   </div>
 
   <div id="popup-img">
-   <img src="media/image12.png" alt="Picture of project members">
+   <img src="media/aboutus/IMG_7916.jpg" alt="Picture of project members">
   </div>
 
-   <button id="close-button-about-us
-">&times;</button>
+   <button id="close-button-about-us">&times;</button>
 
   </div>
-
-  <div id="overlay_aboutUs"></div>`;
+  <div id="overlay_aboutUs"></div>
+  `;
 
   parent.appendChild(aboutUsPopUp);
 };
-
-function renderAboutUsPopUp() {
-  const modal = document.getElementById('about-us-popup');
-  const overlay = document.getElementById('overlay_aboutUs');
-  const openButton = document.getElementById('about-us-button');
-  const closeButton = document.getElementById('close-button-about-us');
-
-  function closeModal(modal) {
-    modal.classList.remove('show');
-    overlay.classList.remove('active');
-  }
-
-  function openModal() {
-    debugger;
-    modal.classList.add('show');
-    overlay.classList.add('active');
-  }
-
-  openButton.addEventListener('click', openModal);
-  closeButton.addEventListener('click', closeModal(modal));
-  overlay.addEventListener('click', closeModal);
-}
